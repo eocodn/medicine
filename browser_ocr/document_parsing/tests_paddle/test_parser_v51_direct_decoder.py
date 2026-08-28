11346b471ee38c5222ec6474ff25f07a4cda63bf
eocodn <neocodn1290@gmail.com>
2026-08-28 12:33:49 +0900

    feat(parser): coordinate v5.1 row slots

--- browser_ocr/document_parsing/tests_paddle/test_parser_v51_direct_decoder.py
+++ browser_ocr/document_parsing/tests_paddle/test_parser_v51_direct_decoder.py
@@ -258,6 +258,8 @@ class ParserV51DirectDecoderTest(unittest.TestCase):
         loss.backward()
         self.assertIsNotNone(model.decoder.row_queries.grad)
         self.assertGreater(float(paddle.abs(model.decoder.row_queries.grad).sum().item()), 0.0)
+        self.assertIsNotNone(model.decoder.row_self_qkv.weight.grad)
+        self.assertGreater(float(paddle.abs(model.decoder.row_self_qkv.weight.grad).sum().item()), 0.0)
         self.assertIsNotNone(model.decoder.node_query.weight.grad)
         self.assertGreater(float(paddle.abs(model.decoder.node_query.weight.grad).sum().item()), 0.0)
         self.assertIsNotNone(model.encoder.text_encoder.embedding.weight.grad)
